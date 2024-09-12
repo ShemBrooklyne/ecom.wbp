21ec470b59de3fc030bc78892c877a96045605f8
Shem Brooklyne Mwangi <shem.mwangi@cellulant.io>
2024-09-12 22:02:08 +0300

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Shop from './components/shop/Shop';
-import FilterSidebar from "./components/sidebar/FilterSidebar";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
